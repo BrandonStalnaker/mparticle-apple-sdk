ff78e2538c6843900793d97ba7c78eab6e2d9e0e
Brandon Stalnaker <33703490+BrandonStalnaker@users.noreply.github.com>
2021-07-08 13:27:45 -0400

    fix: Add Notes

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -1,5 +1,6 @@
 VERSION="$1"
 PREFIXED_VERSION="v$1"
+NOTES="$2"
 
 sed -i '' 's/NSString \*const kMParticleSDKVersion = @".*/NSString *const kMParticleSDKVersion = @"'"$VERSION"'";/' mParticle-Apple-SDK/MPIConstants.m
 /usr/libexec/PlistBuddy -c "Set CFBundleShortVersionString $VERSION" Framework/Info.plist
@@ -14,6 +15,8 @@ git add mParticle-Apple-SDK.podspec
 git add mParticle_Apple_SDK.json
 git add CHANGELOG.md
 git commit -m "chore(release): $VERSION [skip ci]
+
+$NOTES"
 
 ./Scripts/make_artifacts.sh
 ls mParticle_Apple_SDK.framework.zip mParticle_Apple_SDK.framework.nolocation.zip mParticle_Apple_SDK.xcframework.zip mParticle_Apple_SDK.xcframework.nolocation.zip generated-docs.zip || exit 1
